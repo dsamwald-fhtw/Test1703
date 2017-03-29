12f83efdcd7c35e9847f76a3d1222daee4da2cd0
dsamwald <dsamwald@student.tgm.ac.at>
2017-03-29 18:42:26 +0200

    Second commit

--- src/david/Test1703.java
+++ src/david/Test1703.java
@@ -3,5 +3,7 @@ package david;
 public class Test1703 {
 	public static void main(String[] args){
 		System.out.println("Hallo David!");
+		System.out.println("Hallo David!");
+		System.out.println("Hallo David!");
 	}
 }
